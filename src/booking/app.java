fd8149a96ae6e73972ea3fc349d8c61ae1ee2191
matheusMouraDev <mateusmoura12@gmail.com>
2017-04-18 21:59:21 -0300

    Jframe start

--- src/booking/app.java
+++ src/booking/app.java
@@ -5,6 +5,5 @@ public class app {
 
     public static void main(String[] args) {
          
-    }
-    
+    }  
 }
---
+++ src/gui/Controler.java
@@ -0,0 +1,43 @@
+
+package gui;
+
+
+import javax.swing.JFrame;
+
+
+public class Controler extends javax.swing.JFrame {
+
+    public Controler() {
+        initComponents(); 
+    }
+    
+    public static void main(String args[]) {
+        JFrame frame = new JFrame();
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1072, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 642, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
